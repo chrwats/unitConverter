b63c45b1795bd523a7a1249360e13be3945fa8cc
chrwats <chrwats@users.noreply.github.com>
2017-05-18 07:49:11 -0500

    Initial Commit

---
+++ Unit Converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Unit Converter
+//
+//  Created by CHRISTOPHER WATSON on 05/18/17.
+//  Copyright © 2017 CWatson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
